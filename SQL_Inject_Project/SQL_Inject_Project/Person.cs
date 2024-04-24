d556c6c141b5bb6d492a7f48f11d959bb2a786c5
DALindum <thelindum@gmail.com>
2024-04-24 10:11:51 +0200

    push

---
+++ SQL_Inject_Project/SQL_Inject_Project/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_Inject_Project
+{
+    public class Person
+    {
+        public void getAllPersons()
+        {
+            try
+            {
+                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    String sql = "SELECT FirstName, LastName, Age, Email FROM dbo.Person";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void searchFirstName(string firstName)
+        {
+            try
+            {
+                string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    String query = $"SELECT '{firstName}', LastName, Age, Email FROM dbo.Person";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}, {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ SQL_Inject_Project/SQL_Inject_Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using SQL_Inject_Project;
+
+Person person = new Person();
+
+string userInput;
+
+Console.WriteLine(" ");
+Console.WriteLine("Hello, what would you like to know?");
+Console.WriteLine("1: Get All Information of People");
+Console.WriteLine("2: Search first name");
+Console.WriteLine("q: To quit");
+userInput = Console.ReadLine().ToString();
+
+if (userInput == "1")
+{
+    person.getAllPersons();
+}
+if (userInput == "2")
+{
+    Console.WriteLine("What name would you want to search for?");
+    string firstName = Console.ReadLine();
+    person.searchFirstName(firstName);
+}
+if (userInput == "q")
+{
+    return;
+}
